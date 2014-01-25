6d2ee5085d8ed8ab985249015c6419ddb4cb2afb
raphamorim <rapha850@gmail.com>
2014-01-25 02:02:41 -0200

    Try again...

--- js/app.js
+++ js/app.js
@@ -5,23 +5,23 @@ function sayThis(speech)
     url = 'musics.json';
 
     $.ajax({
-  		type: "GET",
-  		url: url,
+        type: "GET",
+        url: url,
         dataType: 'json',
-  		success: function ( response ) {
-  			cont = 0;
+        success: function ( response ) {
+            cont = 0;
 
-  			json = JSON.parse(response);
+            json = response.musics;
 
-    		$.each(json.musics, function(index, music){
-    			if(speech.toUpperCase() == music.Name.toUpperCase()){
-    				alert(music.Name + " - by " + music.Artist);
-    				cont++;
-    			}
+            $.each(json, function(index, music){
+                if(speech.toUpperCase() == music.Name.toUpperCase()){
+                    alert(music.Name + " - by " + music.Artist);
+                    cont++;
+                }
          });
 
          if(cont == 0)
-         	alert("No music found, may not exist in the database.")
+            alert("No music found, may not exist in the database.")
 
          cont = 0;
       }
